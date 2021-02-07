01a8b48bab82fbe6b91ed031b89ac9a6096f8072
laut0104 <miyuyano0104@gmail.com>
2021-02-07 22:19:58 +0900

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import 'next_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+
+  final String title;
+
+
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  final myFocusNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text('test'),
+        actions: <Widget>[
+          Icon(Icons.add),
+          Icon(Icons.share),
+        ],
+      ),
+      body: Container(
+        width: double.infinity,
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Map'),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_album),
+              title: Text('Album'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
---
+++ lib/next_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatelessWidget{
+  NextPage(this.name);
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text('nextpage'),
+
+      ),
+      body: Container(
+        height: double.infinity,
+        color: Colors.red,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(name),
+            Center(
+              child: RaisedButton(
+                child: Text('戻る'),
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+              )
+            ),
+          ],
+        ),
+      ),
+
+    ); //scaffold
+  }
+  
+}
