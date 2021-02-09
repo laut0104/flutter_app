9dcf5447229939ff776c662786939add486db13a
laut0104 <miyuyano0104@gmail.com>
2021-02-09 21:30:48 +0900

    sorce tree使用してみた

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
 
-        title: Text('test'),
+        title: Text('test!!'),
         actions: <Widget>[
           Icon(Icons.add),
           Icon(Icons.share),
