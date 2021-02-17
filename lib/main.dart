6ee54cef07f667f5db40888d141772184043b0f9
laut0104 <miyuyano0104@gmail.com>
2021-02-18 03:02:52 +0900

    途中

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
                         onPressed: (){
                           Navigator.push(
                             context,
-                            MaterialPageRoute(builder: (context) => BookList()),
+                            MaterialPageRoute(builder: (context) => BookListPage()),
                           );
                         },
                       ),
